6331766309698e72a447996de729f9438c7e5f99
Amrit Kumar <amritkum360@gmail.com>
2026-05-31 12:20:41 +0530

    4

--- src/index.js
+++ src/index.js
@@ -34,6 +34,7 @@ app.use(
       "http://127.0.0.1:5173",
       "http://localhost:5174",
       "http://127.0.0.1:5174",
+      "https://ungraded-three.vercel.app/"
     ],
     credentials: true,
   }),
